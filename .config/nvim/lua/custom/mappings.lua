a4d32ea30655af47b00a0c3d619a30807bb80674
Dan Hennessy <danh118@hotmail.co.uk>
2024-04-26 22:35:35 +0100

    mappings

---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.general = {
+  n = {
+	  ["<C-d>"] = { "<C-d>zz", "scroll half-page down and center" },
+	  ["<C-u>"] = { "<C-u>zz", "scroll half-page up and center" },
+  }
+}
+return M
